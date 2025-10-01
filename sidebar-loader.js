d0f8b07a828585cd490e1c0e1100be7e442f1336
toroo3141592 <toramanerenali@gmail.com>
2025-10-01 12:32:55 +0300

    Update sidebar-loader.js

--- sidebar-loader.js
+++ sidebar-loader.js
@@ -5,7 +5,7 @@ async function loadSidebar() {
   let html = localStorage.getItem("sidebar-html");
   if (!html) {
     // yoksa fetch et
-    const res = await fetch("https://github.com/binyirmisekiz/n8n-DijitalDonusumCozumleri/blob/main/sidebar.html");
+    const res = await fetch("https://raw.githubusercontent.com/binyirmisekiz/n8n-DijitalDonusumCozumleri/refs/heads/main/sidebar.html");
     html = await res.text();
     localStorage.setItem("sidebar-html", html);
   }
